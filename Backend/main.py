26a6b3e97f938e0d6dcb924ad872ea0aec5386c4
Angelo <angeloveldbloem1999@gmail.com>
2025-03-08 11:50:53 +0100

    files

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,12 @@
 def test
+analize 
+── main.py
+│── email_parser.py
+│── feature_extractor.py
+│── phishing_detector.py
+│── database.py
+│── logger.py
+│── user_interface.py
+│── utils.py
+│── requirements.txt
+│── README.md
